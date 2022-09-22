bb7da5326b00e77cbaad7864f9b2f19ebe1e8f67
LifeisSoCrazy <rahmanmohammed51@gmail.com>
2022-09-21 22:58:04 -0400

    Many additions made.

---
+++ index.js
@@ -0,0 +1,44 @@
+
+Array = []
+class answer {
+    constructor(a, b, operation) {
+        this.a = a
+        this.b = b
+    }
+    
+    add() {
+        return a + b
+    }
+
+    subtract(a, b) {
+        return a - b
+    }
+
+    multiply(a, b) {
+        return a * b
+    }
+
+    divide(a, b) {
+        return a / b
+    }
+
+    answer (operation, a, b) {
+        if (operation == '+') {
+            add(a, b)
+    }
+    if (operation == '-') {
+        subtract(a. b)
+    }
+    if (operation == '*') {
+        multiply(a, b)
+    }
+    if (operation == '/') {
+        divide(a, b)
+    }
+
+}
+}
+
+
+
+alert(answer('+', 1, 2))
